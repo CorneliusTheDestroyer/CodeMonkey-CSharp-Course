a34cc6635e5fcd55f3eca1036472b0b81ce7cd60
CorneliusTheDestroyer <cornestols7@gmail.com>
2025-03-09 10:26:20 +0200

    New Commit

--- Code Practice/PracticeApp01/PracticeApp01/Program.cs
+++ Code Practice/PracticeApp01/PracticeApp01/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace PracticeApp01
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace PracticeApp01
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        public class Player
         {
-            Console.WriteLine("Hello, World!");
+            string name;
+            int myscore;
+            static int totalScoreAcrossPlayersGoal = 100;
+
+            public Player(string name, int score)
+            {
+                this.name = name;
+                this.myscore = score;
+            }
+
+            public void GetPlayerName()
+            {
+                Console.WriteLine($"Player: {name}, Score: {myscore}");
+            }
+
+            public static void GetTotalScoreAcrossPlayers()
+            {
+                Console.WriteLine($"Total score goal across all players: {totalScoreAcrossPlayersGoal}");
+            }
+        }
+
+        static void Main()
+        {
+            Player player01 = new Player("Marko", 15);
+            Player player02 = new Player("Polo", 20);
+
+            player01.GetPlayerName();
+            player02.GetPlayerName();
+
+            Player.GetTotalScoreAcrossPlayers();
         }
     }
 }
