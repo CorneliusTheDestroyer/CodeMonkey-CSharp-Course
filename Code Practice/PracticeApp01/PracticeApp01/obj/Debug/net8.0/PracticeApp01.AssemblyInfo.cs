8d7370f1cdcfc9b5bf542e8ca330515a61a599bd
CorneliusTheDestroyer <cornestols7@gmail.com>
2025-03-15 12:48:57 +0200

    New commit

--- Code Practice/PracticeApp01/PracticeApp01/obj/Debug/net8.0/PracticeApp01.AssemblyInfo.cs
+++ Code Practice/PracticeApp01/PracticeApp01/obj/Debug/net8.0/PracticeApp01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticeApp01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c98d3816fa9b271594b20bb032b260eaaec731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34cc6635e5fcd55f3eca1036472b0b81ce7cd60")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticeApp01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticeApp01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Code Practice/PracticeApp02 (Beginner concepts practice)/PracticeApp02/PracticeApp02/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+To implement and practice:
+--------------------------
+* If statements
+* Switch
+* Arrays
+* Loops
+* Class
+* Static
+* Access modifiers
+*/
+
+namespace PracticeApp02
+{
+    class Program
+    {
+        private class Customer
+        {
+            private int id;
+            public string name;
+            public string email;
+            public int age;
+            public string city;
+
+            public void HeaderDisplay()
+            {
+                Console.Clear();
+                Console.WriteLine("-------------");
+                Console.WriteLine("Customer Data");
+                Console.WriteLine("-------------");
+            }
+        }
+        static void Main()
+        {
+            string country;
+
+            Customer customer = new Customer();
+
+            Console.WriteLine("Please enter Customer name: ");
+            customer.name = Console.ReadLine();
+
+            Console.WriteLine("Please enter Email address: ");
+            customer.email = Console.ReadLine();
+
+            Console.WriteLine("Please enter Age: ");
+            customer.age = int.Parse(Console.ReadLine());
+
+            switch (customer.city)
+            {
+                case "Joburg":
+                    country = "South Africa";
+                    break;
+                case "New York":
+                    country = "America";
+                    break;
+                case "Sydney":
+                    country = "Australia";
+                    break;
+                default:
+                    break;
+            }
+
+            customer.HeaderDisplay();
+            Console.WriteLine($"Customer name: {customer.name}");
+            Console.WriteLine($"Email address: {customer.email}");
+            Console.WriteLine($"Age: {customer.age}");
+        }
+    }
+}
