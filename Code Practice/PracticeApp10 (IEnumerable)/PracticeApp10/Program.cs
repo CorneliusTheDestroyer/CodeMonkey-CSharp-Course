f3eae84a8d88e150453c561bb7beefabe46eb100
CorneliusTheDestroyer <cornestols7@gmail.com>
2025-03-23 10:49:11 +0200

    New Commit

---
+++ Code Practice/PracticeApp10 (IEnumerable)/PracticeApp10/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * IEnumerable
+ */
+
+namespace PracticeApp10
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Creating a list of strings
+            IEnumerable<string> fruits = new List<string> { "Apple", "Banana", "Cherry" };
+
+            // Using foreach to iterate over the collection
+            foreach (string fruit in fruits) 
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
---
+++ Code Practice/PracticeApp11 (Custom IEnumerable)/PracticeApp11/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Custom IEnumerable
+ */
+
+using System.Collections;
+
+namespace PracticeApp11
+{
+    class Program
+    {
+        class FruitCollection : IEnumerable<string>
+        {
+            private List<string> fruits = new List<string>();
+
+            public void Add(string fruit) 
+            {
+                fruits.Add(fruit);
+            }
+
+            public IEnumerator<string> GetEnumerator()
+            {
+                foreach (string fruit in fruits) 
+                { 
+                    yield return fruit; 
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+        static void Main()
+        {
+            FruitCollection fruits = new FruitCollection();
+
+            fruits.Add("Apple");
+            fruits.Add("Banana");
+            fruits.Add("Peach");
+            fruits.Add("Orange");
+
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+        }
+    }
+}
